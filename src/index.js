c5efa0621a0edc57d87fdd564c5bfb547f818518
Amalcxc <amalcxirsi@gmail.com>
2021-09-21 19:59:08 +0300

    create function to iterate over the tasks array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+const tasks = [
+  {
+    title:'hello', 
+    completed: false, 
+    index: 1
+  },
+  {
+    title:'world',
+    completed: false,
+    index: 2},
+  {
+    title:'bye',
+    completed: false,
+    index: 3
+  }]
+
+ 
+
+const list = document.querySelector('ul')
+  tasks.forEach((item, i)=>{
+    var li = document.createElement("li");
+    li.innerText = tasks[i].title
+    list.appendChild(li)
+    var checkbox = document.createElement("INPUT");
+    checkbox.setAttribute("type", "checkbox");
+    li.appendChild(checkbox)
+    
+  })
+
+  
+

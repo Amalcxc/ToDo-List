2ea0206e0d525baf94db65b0e8df220c75d58591
Amalcxc <amalcxirsi@gmail.com>
2021-09-22 01:37:31 +0300

    localstorge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles.css';
+
 const tasks = [
   {
     title:'hello', 
@@ -15,17 +17,45 @@ const tasks = [
   }]
 
  
+var newtodo = []
+
 
 const list = document.querySelector('ul')
   tasks.forEach((item, i)=>{
     var li = document.createElement("li");
+    li.className = 'list-item';
     li.innerText = tasks[i].title
     list.appendChild(li)
     var checkbox = document.createElement("INPUT");
     checkbox.setAttribute("type", "checkbox");
     li.appendChild(checkbox)
-    
   })
 
-  
 
+
+const input = document.querySelector('.input')
+const btn = document.querySelector(".btn")
+const storedInput = localStorage.getItem('')
+const p = document.querySelector("p")
+var li = document.createElement("li");
+
+if(input){
+  p.textContent = storedInput
+
+}
+
+
+input.addEventListener('input', word => {
+  p.textContent = word.target.value
+})
+
+const savetoLocal = () => {
+  localStorage.setItem('textinput', p.textContent )
+  
+}
+
+btn.addEventListener('click', savetoLocal)
+
+
+
+console.log(newtodo)

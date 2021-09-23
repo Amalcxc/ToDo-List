b64308d3f89d6cddd1f93e53e6756867ced15885
Amalcxc <amalcxirsi@gmail.com>
2021-09-23 20:34:47 +0300

    fix

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import './styles.css';
+import { update, load } from './storage.js';
+
+const tasks = load();
+
+const list = document.querySelector('ul');
+
+tasks.forEach((item, i) => {
+  const li = document.createElement('li');
+  li.className = 'list-item';
+  li.innerText = tasks[i].title;
+  list.appendChild(li);
+  const checkbox = document.createElement('INPUT');
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.checked = tasks[i].completed;
+  checkbox.id = 'checkbox';
+  li.appendChild(checkbox);
+  checkbox.addEventListener('change', function () {
+    if (this.checked) {
+      tasks[i].completed = true;
+      update(tasks);
+    } else {
+      tasks[i].completed = false;
+      update(tasks);
+    }
+  });
+});
+console.log(tasks);
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit');
